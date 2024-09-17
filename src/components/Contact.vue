91e5b94c9cca254a6d278f5c5a8392b9a5e7cf33
Maxime <maxime.rossbach@std.heh.be>
2024-09-17 10:46:26 +0200

    changement couleur text et image

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,13 @@
 <script setup></script>
 <template>
   <div class="pt-10 pb-20">
-    <div>
-      <h1>Contact</h1>
-      <p>Find me on my social networks or send me an email.</p>
+    <div class="pl-2">
+      <h1 class="text-[#D6E1FF]">Contact</h1>
+      <p class="text-[#9EB1FF]">
+        Find me on my social networks or send me an email.
+      </p>
     </div>
-    <div class="pt-10 flex justify-center">
+    <div class="pt-6 flex justify-center">
       <div class="px-4">
         <a
           href="https://www.linkedin.com/in/maxime-rossbach-04b0b023b/"
@@ -16,7 +18,11 @@
       </div>
       <div class="px-4">
         <a href="https://github.com/MaximeRsbch" target="_blank">
-          <img class="w-12" src="../assets/img/git.png" alt="" />
+          <img
+            class="w-12 bg-white rounded-md"
+            src="../assets/img/git.png"
+            alt=""
+          />
         </a>
       </div>
       <div class="px-4">
